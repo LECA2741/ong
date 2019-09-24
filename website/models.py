49bb9953ace4d18448e79657a6faa03846ace652
Alexia <lecaestudante@gmail.com>
2019-09-24 18:26:00 -0300

    continuar projeto ong

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Pessoas(models.model):
+   nome = models.CharField(max_length=255, verbose_name='Nome')
+   
+   sobrenome = models.CharField(max_length=255,
+   verbose_name='sobrenome')
+
+   data_nasc = models.CharField(default=timezone.now, 
+   verbose_name= 'data de nascimento')
+
+   cpf= models.CharField(max_length=11,
+   verbose_name='cpf')
+    cep= models.CharField(max_length=8,
+    verbose_name='cep')
+    item_de_doacao= models.CharField(max_length=255,
+    verbose_name='item de doação')
+    ativo= models.BooleanField(default=True)
+    data_criacao= models.DateTimeField(default=timezone.now)
